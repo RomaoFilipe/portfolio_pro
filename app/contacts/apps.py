66925d2f244b0ade4af1e803a7dbdbeedf68c292
Romão Filipe <150187169+RomaoFilipe@users.noreply.github.com>
2025-06-05 21:12:08 +0100

    Add missing Django boilerplate and templates

--- app/contacts/apps.py
+++ app/contacts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class ContactsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'contacts'
