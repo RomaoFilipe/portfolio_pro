b824f15f87074d0e4d629d7a3766817472e4c4f2
RomaoFilipe <Aluno20923@ipt.pt>
2025-06-06 10:51:44 +0100

    backout

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './app/templates/**/*.html',
+      './app/**/templates/**/*.html',
+      './app/**/*.html',
+      './templates/**/*.html',
+      './**/*.html'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
